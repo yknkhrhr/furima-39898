30e3f6ba79eae9a01fad8c65e88a87bb7a3189c5
yknkhrhr <146395389+yknkhrhr@users.noreply.github.com>
2023-12-12 10:50:37 +0900

    エラーメッセージの表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,24 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :password,
+    format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i,
+    message: 'is invalid. Include both letters and numbers'}
+  validates :last_name,
+    format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters"},
+    presence: true
+  validates :first_name,
+    format:{with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters"},
+    presence: true
+  validates :last_name_kana,
+    format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters"},
+    presence: true
+  validates :first_name_kana,
+    format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters"},
+    presence: true
   validates :birthdate, presence: true
+
+
 
   has_many :items
   has_many :orders
