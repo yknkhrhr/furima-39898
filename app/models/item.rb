4fe21af4c7b6f81c43c7e2dcfe1aba9693b104e5
yknkhrhr <146395389+yknkhrhr@users.noreply.github.com>
2023-12-20 09:20:07 +0900

    バリデーションの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,21 +1,24 @@
 class Item < ApplicationRecord
   with_options presence: true do
+    validates :image
     validates :item_name
     validates :detail
-    validates :price
   end
+  validates :price,
+    format: { with: /\A[0-9]+\z/},
+    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999}
 
   belongs_to :user
   #has_one :order
   has_one_attached :image
-  belongs_to :category
-  belongs_to :status
-  belongs_to :fee_burden
-  belongs_to :prefecture
-  belongs_to :send_day
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-    with_options numericality: { othe_than:1, message: "can't be blank"} do
+    belongs_to :category
+    belongs_to :status
+    belongs_to :fee_burden
+    belongs_to :prefecture
+    belongs_to :send_day
+    with_options numericality: { other_than: 1 , message: "can't be blank"} do
       validates :category_id
       validates :status_id
       validates :fee_burden_id
