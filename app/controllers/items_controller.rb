abea7054ced566edb684706fdbcf8e18769e5318
yknkhrhr <146395389+yknkhrhr@users.noreply.github.com>
2023-12-20 09:20:47 +0900

    軽微な修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,11 +9,16 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
   def item_params
-    params.require(:item).permit(:item_name, :detail).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :item_name, :detail, :category_id, :status_id, :fee_burden_id, :prefecture_id, :send_day_id, :price).merge(user_id: current_user.id)
   end
 
 
