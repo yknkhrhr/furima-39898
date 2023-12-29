a2caad54414880de610ad7c44d5196c81d89466f
yknkhrhr <146395389+yknkhrhr@users.noreply.github.com>
2023-12-29 21:13:56 +0900

    コントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -46,6 +46,8 @@ class ItemsController < ApplicationController
     if user_signed_in? && current_user.id == @item.user_id 
       @item.destroy
       redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
