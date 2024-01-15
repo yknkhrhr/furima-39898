6cc384f5ea049e0e9a8d224bebd12027a42d0a1c
yknkhrhr <146395389+yknkhrhr@users.noreply.github.com>
2024-01-16 03:54:45 +0900

    User・Itemモデルの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,7 @@ class Item < ApplicationRecord
     numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999}
 
   belongs_to :user
-  #has_one :order
+  has_one :order
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
@@ -17,7 +17,7 @@ class Item < ApplicationRecord
     belongs_to :fee_burden
     belongs_to :prefecture
     belongs_to :send_day
-    with_options numericality: { other_than: 1 , message: "can't be blank"} do
+    with_options numericality: { other_than: 0 , message: "can't be blank"} do
       validates :category_id
       validates :status_id
       validates :fee_burden_id
